c37c979792e6f4da20adf706092c06d3635d8e59
Aleksey Bershov <bershov.aleksey@bk.ru>
2023-06-21 15:41:16 +0300

    intermediate commit

--- src/Main.java
+++ src/Main.java
@@ -11,50 +11,44 @@ public class Main {
 
     public static void main(String[] args) {
 
-        List<String> listDirGame = Arrays.asList("src", "res", "savegames", "temp");
-        for (String nameDir : listDirGame) {
-            directoryCreationMethod("C://Games", nameDir);
+        List<String> listDir
+                = Arrays.asList("src", "res", "savegames", "temp", "main", "test", "drawables", "vectors", "icons");
+        for (String nameDir : listDir) {
+            if (nameDir.equals("src") || nameDir.equals("res") || nameDir.equals("savegames") || nameDir.equals("temp")) {
+                directoryCreationMethod("C://Games/" + nameDir);
+            }
+            if (nameDir.equals("main") || nameDir.equals("test")) {
+                directoryCreationMethod("C://Games/src/" + nameDir);
+            }
+            if (nameDir.equals("drawables") || nameDir.equals("vectors") || nameDir.equals("icons")) {
+                directoryCreationMethod("C://Games/res/" + nameDir);
+            }
         }
 
         fileCreationMethod("C://Games/temp", "temp.txt");
-
-        List<String> listDirSrc = Arrays.asList("main", "test");
-        for (String nameDir : listDirSrc) {
-            directoryCreationMethod("C://Games/src", nameDir);
-        }
-
-        List<String> listDirRes = Arrays.asList("drawables", "vectors", "icons");
-        for (String nameDir : listDirRes) {
-            directoryCreationMethod("C://Games/res", nameDir);
-        }
-
 
         List<String> listFileDirMain = Arrays.asList("Main.java", "Utils.java");
         for (String nameFile : listFileDirMain) {
             fileCreationMethod("C://Games/src/main", nameFile);
         }
 
-        // проверка на ошибку создания файла, пишется в лог (ошибка пути)
-        fileCreationMethod("C://Games/temp/1", "temp1.txt");
-
         logWrite(stringBuilder, "C://Games/temp/temp.txt");
-
     }
 
-    public static void directoryCreationMethod(String path, String nameDir) {
+    public static void directoryCreationMethod(String path) {
 
         Date date = new Date();
-        File dir = new File(path, nameDir);
+        File dir = new File(path);
         if (dir.exists()) {
-            stringBuilder.append(date.toString() + " - Попытка создания директории " + nameDir + ", директория "
-                    + nameDir + " уже существует!");
+            stringBuilder.append(date.toString() + " - Попытка создания директории по пути " + path + ", директория по данному пути "
+                    + path + ": уже существует!");
             stringBuilder.append("\r\n");
         } else {
             if (dir.mkdir()) {
-                stringBuilder.append(date.toString() + " - Директория " + nameDir + " создана.");
+                stringBuilder.append(date.toString() + " - Директория по абсолютному пути " + path + ": создана.");
                 stringBuilder.append("\r\n");
             } else {
-                stringBuilder.append(date.toString() + " - Директория " + nameDir + " не создана!");
+                stringBuilder.append(date.toString() + " - Директория по абсолютному пути " + path + ": не создана!");
                 stringBuilder.append("\r\n");
             }
         }
@@ -66,16 +60,16 @@ public class Main {
         File file = new File(path, nameFile);
         if (file.exists()) {
             stringBuilder.append(date.toString() + " - Попытка создания файла " + nameFile +
-                    ", файл " + nameFile + " уже существует!");
+                    ", файл " + nameFile + ": уже существует!");
             stringBuilder.append("\r\n");
         } else {
             try {
                 if (file.createNewFile()) {
-                    stringBuilder.append(date.toString() + " - Файл " + nameFile + " создан.");
+                    stringBuilder.append(date.toString() + " - Файл " + nameFile + ": создан.");
                     stringBuilder.append("\r\n");
                 }
             } catch (IOException e) {
-                stringBuilder.append(date.toString() + " - Файл " + nameFile + " не создан! " + e.getMessage() + ".");
+                stringBuilder.append(date.toString() + " - Файл " + nameFile + ": не создан! " + e.getMessage() + ".");
                 stringBuilder.append("\r\n");
             }
         }
